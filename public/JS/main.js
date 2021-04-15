8e99f0764cf5409053e0053f6cfbbcf90ef0484a
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-04-15 12:03:18 +0200

    fini

---
+++ public/JS/main.js
@@ -0,0 +1,27 @@
+// Exo 1
+let exo1 = "J’aime les tartines et j’en mange";
+console.log(exo1);
+let exo2 = "4 par jours";
+console.log(exo1 + " " + exo2);
+let exo3 = 5;
+let exo4 = 35;
+console.log(exo3 + exo4);
+console.log(exo1 + " " + exo3);
+
+//Exo 2
+let Ex1 = "Salut tout le monde";
+console.log(Ex1);
+let Ex2 = "Aurevoir !";
+console.log(Ex2);
+Ex1 = Ex2;
+console.log(Ex1);
+
+//Exo 3
+let e1 = 4;
+let e2 = "6";
+console.log(e1>e2);
+console.log(e1<e2);
+console.log(e1==e2);
+console.log(e1!=e2);
+console.log(e1===e2);
+console.log(e1!==e2);
